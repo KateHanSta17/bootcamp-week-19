c7348f8fb7835d10b24d81cf5e830e5572efff2d
Hank Star <kate.stapley@hotmail.com>
2024-10-01 19:16:27 +1000

    activity 2 and 4

--- 04-Stu_Webpack-Intro/Unsolved/src/js/index.js
+++ 04-Stu_Webpack-Intro/Unsolved/src/js/index.js
@@ -1,5 +1,6 @@
 // TODO: Add a comment explaining role of the index.js file and import statements
-
+// the role of the index.js file is to import the functions from the other files 
+// and then add event listeners to the buttons that call the functions when clicked. 
 import { boxClick } from './box';
 import { headerClick } from './header';
 
